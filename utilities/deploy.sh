c91d6424a26d4fe5b1b87fdc35db6633585d3dbc
Shankar Kulumani <shanks.k@gmail.com>
2017-12-20 21:51:00 -0500

    Setup travis

---
+++ utilities/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# deploy to github tagged releases
+if [ -n "$TRAVIS_TAG" ]; then
+    echo "Tag $TRAVIS_TAG exists"
+else
+    echo "No current tag. Lets make one"
+    git config --local user.name 'travis'
+    git config --local user.email 'travis'
+    git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag"
+
+    echo "Done making a tag"
+fi
